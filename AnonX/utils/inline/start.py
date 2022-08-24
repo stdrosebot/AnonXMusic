ac1db0ea83f7700a55e1662af0f16af61beb21db
SHIKHA <108600094+SHIKHA-ROBOT@users.noreply.github.com>
2022-08-24 16:44:04 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -27,7 +27,7 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             InlineKeyboardButton(
                 text="ᴍᴀɪɴᴛᴀɪɴᴇʀ", user_id=OWNER),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url=f"{config.SUPPORT_GROUP}"
+                text="ɴᴇᴛᴡᴏʀᴋ", url=f"{config.SUPPORT_GROUP}"
             ),
         ],
      ]
@@ -50,7 +50,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         [
             InlineKeyboardButton(text="ᴍᴀɪɴᴛᴀɪɴᴇʀ", user_id=OWNER),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url=f"{config.SUPPORT_GROUP}"
+                text="ɴᴇᴛᴡᴏʀᴋ", url=f"{config.SUPPORT_GROUP}"
             ),
         ],
         [
