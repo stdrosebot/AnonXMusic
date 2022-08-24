3c811ce8b4b9be6b55fb5c8b22ef5e72a7aba13f
SHIKHA <108600094+SHIKHA-ROBOT@users.noreply.github.com>
2022-08-24 17:13:26 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -43,9 +43,9 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/Team_STD_Network")
+    "SUPPORT_CHANNEL", "https://t.me/STD_FRIENDS_CLUB")
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/STD_FRIENDS_CLUB")
+    "SUPPORT_GROUP", "https://t.me/Team_STD_Network")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
